798f8369d11125c98d9987b558ab6fbf3fcad30a
ravsil <rafaelvieiradas@gmail.com>
2024-09-04 15:35:09 -0300

    fixed ctrl+c and added colors

--- main.c
+++ main.c
@@ -2,59 +2,76 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
-int changeDir(char *buffer, char *curDir)
+#define RED "\033[1;31m\e[1m"
+#define BLUE "\033[0;34m\e[1m"
+#define GREEN "\033[0;32m\e[1m"
+#define RESET "\033[0m"
+
+void printDir(char *curDir)
+{
+	printf(BLUE);
+	printf("%s", getcwd(curDir, 255));
+	printf(GREEN);
+	printf("|>> ");
+	printf(RESET);
+}
+
+void changeDir(char *buffer, char *curDir)
 {
 	if (chdir(buffer) == -1)
 	{
+		printf(RED);
 		printf("Erro: caminho '%s' nao encontrado\n", buffer);
+		printf(RESET);
 	}
-	else
-	{
-		printf("%s|>> ", getcwd(curDir, 255));
-	}
+	printDir(curDir);
 }
 
-void problems(char *curDir)
+void help(char *curDir)
 {
 	printf("Se o comando 'cd' for executado juntamente de outro comando, o segundo argumento nao funcionara\n");
 	printf("caso1: echo hello && cd ..\n");
 	printf("'cd ..' nao sera executado\n");
 	printf("caso2: cd .. && echo hello\n");
 	printf("'echo hello' nao sera executado\n\n");
-	printf("Se um programa como python for executado, o input do teclado sera extremamente prejudicado\n\n");
-	printf("Se um comando que eh executado constantemente (como 'ping') for acionado, ao parar sua execucao o terminal tambem sera encerrado\n");
-	printf("%s|>> ", getcwd(curDir, 255));
+	printf("Alguns programas como vi nao funcionam como deveriam\n\n");
+	printDir(curDir);
 }
 
 int main()
 {
-	printf("Terminal\n");
+	// ignore ctrl+c
+	signal(SIGINT, SIG_IGN);
+
 	char buffer[255];
 	char curDir[255];
 	int id = -1;
-	printf("Digite 'problemas' para saber os problemas conhecidos\n");
-	printf("%s|>> ", getcwd(curDir, 255));
+
+	printf("Digite 'ajuda' para ajuda\n");
+	printDir(curDir);
 	while (1)
 	{
 		// child id == 0
 		if (id == 0)
 		{
 			system(buffer);
-			printf("%s|>> ", getcwd(curDir, 255));
+			printDir(curDir);
 			break;
 		}
 		else
 		{
 			fgets(buffer, 255, stdin);
+			// remove \n
 			buffer[strlen(buffer) - 1] = '\0';
 			if (buffer[0] == 'c' && buffer[1] == 'd' && buffer[2] == ' ')
 			{
 				changeDir(buffer + 3, curDir);
 			}
-			else if (strcmp(buffer, "problemas") == 0)
+			else if (strcmp(buffer, "ajuda") == 0)
 			{
-				problems(curDir);
+				help(curDir);
 			}
 			else if (strcmp(buffer, "exit") == 0)
 			{
